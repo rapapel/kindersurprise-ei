6d0473413878f0f22dee01beeb17845d11389906
Raphaël <raphaelofficieel@gmail.com>
2024-11-08 09:13:46 +0100

    papa

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -6,29 +6,25 @@ $password = $_POST['password'];
 
 require_once '../../../config/conn.php';
 
-try {
-    $query = "SELECT * FROM users WHERE username = :username";
-    $statement = $conn->prepare($query);
-    $statement->execute([
-        ":username" => $gebruikersnaam,
-    ]);
-    $user = $statement->fetch(PDO::FETCH_ASSOC);
+$query = "SELECT * FROM users WHERE username = :username";
+$statement = $conn->prepare($query);
+$statement->execute([
+    ":username" => $gebruikersnaam,
+]);
+$user = $statement->fetch(PDO::FETCH_ASSOC);
 
-    if ($statement->rowCount() < 1) {
-        die("Error: account bestaat niet!");
-    }
-
-    if (!password_verify($password, $user['password'])) {
-        die("Error: wachtwoord onjuist!");
-    }
-
-    $_SESSION['user_id'] = $user['id'];
-    $_SESSION['user_name'] = $user['username'];
-
-    $msg = "Je bent ingelogd!";
-    header("Location: $base_url/resources/views/taken/index.php?msg=$msg");
-    exit;
-} catch (PDOException $e) {
-    die("Error: " . $e->getMessage());
+if ($statement->rowCount() < 1) {
+    die("Error: account bestaat niet!");
 }
+
+if (!password_verify($password, $user['password'])) {
+    die("Error: wachtwoord onjuist!");
+}
+
+$_SESSION['user_id'] = $user['id'];
+$_SESSION['user_name'] = $user['username'];
+
+$msg = "Je bent ingelogd!";
+header("Location: $base_url/resources/views/taken/index.php?msg=$msg");
+exit;
 ?>
