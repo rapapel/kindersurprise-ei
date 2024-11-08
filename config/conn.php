7cf42903c93882d1a7e236768c533bed8ab6f8e8
d296063 <d296063@edu.curio.nl>
2024-11-08 09:23:06 +0100

    conn.php aangepast

--- config/conn.php
+++ config/conn.php
@@ -1,7 +1,19 @@
 <?php
 
 //Haal de configuratie op
-require_once './../backend/config.php';
+
+#### TODO
+//Hernoem dit bestand naar 'config.php' en vul jouw eigen database-gegevens in.
+//Deze config wordt hierna _niet_ meegestuurd naar je groepsgenoten. Zo kan iedereen zijn eigen wachtwoord, etc. invullen.
+
+$dbHost = 'localhost';
+$dbName = 'takenlijst';
+$dbUser = 'root';
+$dbPass = '';
+
+//De url waarop jouw project staat. Géén slash aan het einde.
+$base_url = 'http://localhost/kindersurprise-ei';
+
 
 //Met behulp van PDO zetten we de connectie op, waarna we met setAttribute de manier van errormeldingen weergeven bepalen.
 $conn = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
